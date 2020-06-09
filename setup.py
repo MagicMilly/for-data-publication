ab029611e28169ca41f6ca9050bc748a158fbb50
MagicMilly <emilycain@ymail.com>
2020-06-09 10:35:23 -0700

    initialize new cookiecutter project repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='TThis project contains source code, other related documents, and small, derived datasets for the NSF-funded GenoPhenoEnvo project. This repo supports one of our goals to provide open data and reproducible code in order to follow FAIR data principles and contribute to open science.',
+    author='Emily Cain',
+    license='MIT',
+)
